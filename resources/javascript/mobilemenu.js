02956b8e2549f7ac7cefe735a42565c25aee5bab
Chaunte <chauntevandeburgh@gmail.com>
2018-05-25 14:40:06 -0500

    add close icon to aluminum

--- resources/javascript/mobilemenu.js
+++ resources/javascript/mobilemenu.js
@@ -1,4 +1,13 @@
 const mobileMenu = () => {
 	document.getElementById('sidebar').style.display = "flex";
 	document.getElementById('banner').style.display = "none";
+	document.getElementById('menu-icon').style.display = "none";
+	document.getElementById('menu-close').style.display = "flex";
+};
+
+const closeMenu = () => {
+	document.getElementById('sidebar').style.display = "none";
+	document.getElementById('banner').style.display = "flex";
+	document.getElementById('menu-icon').style.display = "flex";
+	document.getElementById('menu-close').style.display = "none";
 };
